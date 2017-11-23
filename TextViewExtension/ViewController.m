63bbac32701cf8b9bd8333642d3f6f2fe71cdb3d
XSDCoder <i@xsd.me>
2017-11-23 10:07:19 +0800

    Fix bug. Version 1.0.1

--- TextViewExtension/ViewController.m
+++ TextViewExtension/ViewController.m
@@ -18,8 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     UITextField *textField = [UITextField setupTextFieldWithTextColor:[UIColor darkTextColor] textSize:18 placeholderColor:[UIColor cyanColor] maxLength:100 returnKeyType:UIReturnKeyDone keyboardType:UIKeyboardTypeDefault clearButtonMode:UITextFieldViewModeWhileEditing addView:self.view];
+    textField.disEmoji = YES;
     textField.frame = CGRectMake(30, 30, 300, 50);
-//    textField.backgroundColor = [UIColor yellowColor];
+    textField.backgroundColor = [UIColor yellowColor];
 }
 
 
--- XSDTextViewExtension/XSDTextViewExtension.m
+++ XSDTextViewExtension/XSDTextViewExtension.m
@@ -41,23 +41,21 @@ static const char XSDTextViewExtensionDisEmojiKey = '\0';
 
 - (void)textDidChange:(NSNotification *)notification {
     UITextField *textField = notification.object;
-    if ([textField respondsToSelector:@selector(disEmoji)]) {
-        if (textField.disEmoji) {
-            textField.text = [XSDTextViewExtension converStrEmoji:textField.text];
-        }
-        if (textField.maxLength) {
-            NSString *toBeString = textField.text;
-            UITextRange *selectedRange = [textField markedTextRange];
-            UITextPosition *position = [textField positionFromPosition:selectedRange.start offset:0];
-            if (!position || !selectedRange) {
-                if (toBeString.length > textField.maxLength) {
-                    NSRange rangeIndex = [toBeString rangeOfComposedCharacterSequenceAtIndex:textField.maxLength];
-                    if (rangeIndex.length == 1) {
-                        textField.text = [toBeString substringToIndex:textField.maxLength];
-                    } else {
-                        NSRange rangeRange = [toBeString rangeOfComposedCharacterSequencesForRange:NSMakeRange(0, textField.maxLength)];
-                        textField.text = [toBeString substringWithRange:rangeRange];
-                    }
+    if ([textField respondsToSelector:@selector(disEmoji)] && textField.disEmoji) {
+        textField.text = [XSDTextViewExtension converStrEmoji:textField.text];
+    }
+    if ([textField respondsToSelector:@selector(disEmoji)] && textField.maxLength) {
+        NSString *toBeString = textField.text;
+        UITextRange *selectedRange = [textField markedTextRange];
+        UITextPosition *position = [textField positionFromPosition:selectedRange.start offset:0];
+        if (!position || !selectedRange) {
+            if (toBeString.length > textField.maxLength) {
+                NSRange rangeIndex = [toBeString rangeOfComposedCharacterSequenceAtIndex:textField.maxLength];
+                if (rangeIndex.length == 1) {
+                    textField.text = [toBeString substringToIndex:textField.maxLength];
+                } else {
+                    NSRange rangeRange = [toBeString rangeOfComposedCharacterSequencesForRange:NSMakeRange(0, textField.maxLength)];
+                    textField.text = [toBeString substringWithRange:rangeRange];
                 }
             }
         }
@@ -69,59 +67,9 @@ static const char XSDTextViewExtensionDisEmojiKey = '\0';
 }
 
 + (NSString *)converStrEmoji:(NSString *)emojiStr {
-    NSString *tempStr = [[NSString alloc] init];
-    NSMutableString *kksstr = [[NSMutableString alloc] init];
-    NSMutableArray *array = [NSMutableArray arrayWithCapacity:10];
-    NSMutableString *strMu = [[NSMutableString alloc] init];
-    for(int i =0; i < [emojiStr length]; i++) {
-        tempStr = [emojiStr substringWithRange:NSMakeRange(i, 1)];
-        [strMu appendString:tempStr];
-        if ([self stringContainsEmoji:strMu]) {
-            strMu = [[strMu substringToIndex:([strMu length] - strMu.length > 1 ? 2 : strMu.length)] mutableCopy];
-            [array removeLastObject];
-            continue;
-        }else
-            [array addObject:tempStr];
-    }
-    for (NSString *strs in array) {
-        [kksstr appendString:strs];
-    }
-    return kksstr;
-}
-
-// 判断字符串是否为表情
-+ (BOOL)stringContainsEmoji:(NSString *)string {
-    __block BOOL returnValue = NO;
-    [string enumerateSubstringsInRange:NSMakeRange(0, [string length]) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
-         const unichar hs = [substring characterAtIndex:0];
-         if (0xd800 <= hs && hs <= 0xdbff) {
-             if (substring.length > 1) {
-                 const unichar ls = [substring characterAtIndex:1];
-                 const int uc = ((hs - 0xd800) * 0x400) + (ls - 0xdc00) + 0x10000;
-                 if (0x1d000 <= uc && uc <= 0x1f77f) {
-                     returnValue = YES;
-                 }
-             }
-         } else if (substring.length > 1) {
-             const unichar ls = [substring characterAtIndex:1];
-             if (ls == 0x20e3) {
-                 returnValue = YES;
-             }
-         } else {
-             if (0x2100 <= hs && hs <= 0x27ff) {
-                 returnValue = YES;
-             } else if (0x2B05 <= hs && hs <= 0x2b07) {
-                 returnValue = YES;
-             } else if (0x2934 <= hs && hs <= 0x2935) {
-                 returnValue = YES;
-             } else if (0x3297 <= hs && hs <= 0x3299) {
-                 returnValue = YES;
-             } else if (hs == 0xa9 || hs == 0xae || hs == 0x303d || hs == 0x3030 || hs == 0x2b55 || hs == 0x2b1c || hs == 0x2b1b || hs == 0x2b50) {
-                 returnValue = YES;
-             }
-         }
-     }];
-    return returnValue;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"[^\\u0020-\\u007E\\u00A0-\\u00BE\\u2E80-\\uA4CF\\uF900-\\uFAFF\\uFE30-\\uFE4F\\uFF00-\\uFFEF\\u0080-\\u009F\\u2000-\\u201f\r\n]" options:NSRegularExpressionCaseInsensitive error:nil];
+    NSString *modifiedString = [regex stringByReplacingMatchesInString:emojiStr options:0 range:NSMakeRange(0, [emojiStr length]) withTemplate:@""];
+    return modifiedString;
 }
 
 @end
